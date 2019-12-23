60494b97837c063a282c2b62aa570ccf8815d77c
phatdroid94 <phatdroid94@gmail.com>
2019-12-23 20:28:16 +0700

    fragment manager not async

--- app/src/main/java/com/example/fragmentransaction04092019/MainActivity.java
+++ app/src/main/java/com/example/fragmentransaction04092019/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +24,41 @@ public class MainActivity extends AppCompatActivity {
     public void addAndroid(View view) {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         AndroidFragment androidFragment = new AndroidFragment();
-        fragmentTransaction.add(R.id.liearlayoutContainer,androidFragment);
+        fragmentTransaction.add(R.id.liearlayoutContainer, androidFragment);
         fragmentTransaction.commit();
     }
+
     public void addIos(View view) {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         IosFragment iosFragment = new IosFragment();
-        fragmentTransaction.add(R.id.liearlayoutContainer,iosFragment);
+        fragmentTransaction.add(R.id.liearlayoutContainer, iosFragment);
         fragmentTransaction.commit();
     }
+
+    public void replaceAndroid(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.replace(R.id.liearlayoutContainer, androidFragment);
+        fragmentTransaction.commit();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("BBB", fragmentManager.getFragments().size() + "");
+            }
+        },2000);
+    }
+
+
+    public void replaceIos(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        IosFragment iosFragment = new IosFragment();
+        fragmentTransaction.replace(R.id.liearlayoutContainer, iosFragment);
+        fragmentTransaction.commit();
+    }
+    public void removeAndroid(View view) {
+    }
+    public void removeIos(View view) {
+    }
+
+
 }
