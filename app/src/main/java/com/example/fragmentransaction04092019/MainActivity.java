4ec3b20fed92d25f2cffa03fc653b69f63e98722
phatdroid94 <phatdroid94@gmail.com>
2019-12-23 20:15:14 +0700

    add fragment

--- app/src/main/java/com/example/fragmentransaction04092019/MainActivity.java
+++ app/src/main/java/com/example/fragmentransaction04092019/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fragmentransaction04092019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +10,24 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
     }
 
     public void addAndroid(View view) {
-
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.add(R.id.liearlayoutContainer,androidFragment);
+        fragmentTransaction.commit();
+    }
+    public void addIos(View view) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        IosFragment iosFragment = new IosFragment();
+        fragmentTransaction.add(R.id.liearlayoutContainer,iosFragment);
+        fragmentTransaction.commit();
     }
 }
